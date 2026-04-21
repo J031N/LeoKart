00a94971f5d912700ca85edd6e2c3303db262730
Jobin Joseph <jobinj2512@gmail.com>
2026-04-21 11:33:07 +0530

    static fix

--- brocart/settings.py
+++ brocart/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'brocart.urls'
@@ -129,3 +130,5 @@ MEDIA_ROOT=os.path.join( BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
